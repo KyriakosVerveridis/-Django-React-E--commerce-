f0e5fe6ab5a6f32bc019d11ff3419080e38d1f3b
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-12-03 23:34:50 +0200

    Implementing React/Bootstrap Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
   return (
 	<header>
 		<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 			<Container>
-				<Navbar.Brand href="/">WhiteLabelShop</Navbar.Brand>
+				<LinkContainer to="/">
+					<Navbar.Brand>WhiteLabelShop</Navbar.Brand>
+				</LinkContainer>
+				
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="mr-auto">
-						<Nav.Link href="/card"><i className='fas fa-shopping-cart'></i>Card</Nav.Link>
-						<Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+						<LinkContainer to="/card">
+							<Nav.Link><i className='fas fa-shopping-cart'></i>Card</Nav.Link>
+						</LinkContainer>
+						
+						<LinkContainer to="/login">
+							<Nav.Link><i className='fas fa-user'></i>Login</Nav.Link>
+						</LinkContainer>
+						
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
