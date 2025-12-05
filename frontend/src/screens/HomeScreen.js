e24103e2c27fa7c43ca50254f8d576902f7c7ab3
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-12-05 20:58:04 +0200

    Database Setup

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect }from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import axios from 'axios';
+import Rating from '../components/Rating';
 
 
 function HomeScreen() {
