db280e32747edee31337eb3c40f5ef9881fcdd77
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2026-01-14 00:09:38 +0200

    JWT Customization

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
@@ -7,7 +8,24 @@ from .models import Product
 from .products import products
 from .serializers import ProductSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 # Create your views here.
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs: dict[str, Any]) -> dict[str, str]:
+        data = super().validate(attrs)
+
+        data["username"] = self.user.username
+        data["email"] = self.user.email
+
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view (['GET'])
 def getRouter(request):
