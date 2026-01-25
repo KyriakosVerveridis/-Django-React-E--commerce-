7ebfa151319d8371704e70a6eab06e56a824383e
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2026-01-25 22:14:01 +0200

    Feat: Urls And Views Cleanup

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,23 +1,18 @@
-from typing import Any
 from django.shortcuts import render
-from django.http import JsonResponse
+from typing import Any
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product 
 from django.contrib.auth.models import User
-from .products import products
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
-
-
-# Create your views here.
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -51,13 +46,7 @@ def registerUser(request):
     except:
         message = {'detail': 'User with this email already exists.'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view (['GET'])
-def getRouter(request):
     
-    return Response("Hello")
-
 
 @api_view (['GET'])
 @permission_classes([IsAuthenticated])
@@ -69,29 +58,10 @@ def getUserProfile(request):
     serializer = UserSerializer(user, many=False) # serialize a single user object
     return Response(serializer.data) # return data as a JSON object
 
+
 @api_view (['GET'])
 @permission_classes([IsAdminUser])
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-
-@api_view (['GET'])
-def getProducts(request):
-    """
-    Function to fetch all products.
-    """
-    products = Product.objects.all() # get all products from DB
-    serializer = ProductSerializer(products, many=True) # serialize a list of objects
-    return Response(serializer.data)
-
-
-@api_view (['GET'])
-def getProduct(request, pk):
-    """
-    Function to fetch a specific product by ID.
-    """
-    product = Product.objects.get(_id=pk) # get product from DB
-    serializer = ProductSerializer(product, many=False) # serialize a single object
-    return Response(serializer.data)
+    return Response(serializer.data)    
