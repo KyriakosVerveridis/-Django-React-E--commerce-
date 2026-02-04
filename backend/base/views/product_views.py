90537713f09f20bc61b253249483748d03067ec5
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2026-02-04 21:15:01 +0200

    Fix merge conflict and finalize image upload view

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -43,3 +43,19 @@ def deleteProduct(request, pk):
     productToDelete.delete()
     
     return Response('Product deleted')
+
+
+@api_view (['POST'])
+def uploadImage(request):
+    """
+    Function to handle image upload for products.
+    """
+    data = request.data
+
+    product_id = data.get('product_id')
+    product = Product.objects.get(_id=product_id)
+
+    product.image = request.FILES.get('image')
+    product.save()
+
+    return Response('Image was uploaded')
